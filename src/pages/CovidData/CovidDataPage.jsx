d3f69e45a3a5f091d6caa4547102673a246a099d
Shubham-Praj <shubhampraj11@gmail.com>
2023-03-07 01:45:56 +0530

    covid page layout

--- src/pages/CovidData/CovidDataPage.jsx
+++ src/pages/CovidData/CovidDataPage.jsx
@@ -1,7 +1,23 @@
+import { Box } from "@mui/material";
 import React from "react";
+import { CovidDataTable } from "./CovidDataTable";
 
 function CovidDataPage() {
-  return <div>index</div>;
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box>Filters</Box>
+
+      <Box sx={{ display: "flex", flexDirection: "row" }}>
+        <Box>Cards Section</Box>
+        <Box>Chart</Box>
+      </Box>
+
+      <Box>
+        Table
+        <CovidDataTable />
+      </Box>
+    </Box>
+  );
 }
 
 export default CovidDataPage;
