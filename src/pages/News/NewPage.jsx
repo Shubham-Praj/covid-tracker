c04e77da98552f9ba8a7b509105bee52ede7ee6e
Shubham-Praj <shubhampraj11@gmail.com>
2023-03-07 00:42:12 +0530

    pagination and page loader added

--- src/pages/News/NewPage.jsx
+++ src/pages/News/NewPage.jsx
@@ -1,4 +1,11 @@
-import { Box, Grid, Tab, Tabs } from "@mui/material";
+import {
+  Box,
+  CircularProgress,
+  Grid,
+  Pagination,
+  Tab,
+  Tabs,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { authHeader } from "../../services/HeaderService";
 import NewsCard from "./NewCard";
@@ -27,6 +34,8 @@ function a11yProps(index) {
 }
 
 function NewPage() {
+  const [loaderOnPage, setLoaderOnPage] = useState(null);
+
   const [allCovidTableData, setAllCovidTableData] = useState([]);
   const [allHealthTableData, setAllHealthTableData] = useState([]);
   const [allVaccineTableData, setAllVaccineTableData] = useState([]);
@@ -37,41 +46,53 @@ function NewPage() {
     setValue(newValue);
   };
 
-  const getAllCovidNewsData = async () => {
+  const getAllCovidNewsData = async (pageNumber) => {
+    setLoaderOnPage("covid-news");
+
     const res = await fetch(
-      `${process.env.REACT_APP_COVID_BASE_URL}${process.env.REACT_APP_ALL_COVID_NEWS}`,
+      `${process.env.REACT_APP_COVID_BASE_URL}${process.env.REACT_APP_ALL_COVID_NEWS}/${pageNumber}`,
       authHeader()
     );
 
     const data = await res.json();
+
     setAllCovidTableData(data.news);
+    setLoaderOnPage(null);
   };
 
-  const getAllHealthNewsData = async () => {
+  const getAllHealthNewsData = async (pageNumber) => {
+    setLoaderOnPage("health-news");
+
     const res = await fetch(
-      `${process.env.REACT_APP_COVID_BASE_URL}${process.env.REACT_APP_ALL_HEALTH_NEWS}`,
+      `${process.env.REACT_APP_COVID_BASE_URL}${process.env.REACT_APP_ALL_HEALTH_NEWS}/${pageNumber}`,
       authHeader()
     );
 
     const data = await res.json();
     setAllHealthTableData(data.news);
+    setLoaderOnPage(null);
   };
 
-  const getAllVaccineNewsData = async () => {
+  const getAllVaccineNewsData = async (pageNumber) => {
+    setLoaderOnPage("vaccine-news");
+
     const res = await fetch(
-      `${process.env.REACT_APP_COVID_BASE_URL}${process.env.REACT_APP_ALL_VACCINE_NEWS}`,
+      `${process.env.REACT_APP_COVID_BASE_URL}${process.env.REACT_APP_ALL_VACCINE_NEWS}/${pageNumber}`,
       authHeader()
     );
 
     const data = await res.json();
 
-    setAllVaccineTableData(data.news);
+    if (data.hasOwnProperty("news")) {
+      setAllVaccineTableData(data.news);
+    }
+    setLoaderOnPage(null);
   };
 
   useEffect(() => {
-    getAllCovidNewsData();
-    getAllHealthNewsData();
-    getAllVaccineNewsData();
+    getAllCovidNewsData(0);
+    getAllHealthNewsData(0);
+    getAllVaccineNewsData(0);
   }, []);
 
   return (
@@ -92,42 +113,87 @@ function NewPage() {
       </Box>
 
       <TabPanel value={value} index={0}>
-        <Grid container rowSpacing={2} columnSpacing={2} sx={{ p: 2 }}>
-          {allCovidTableData?.length > 0 &&
-            allCovidTableData?.map((news) => {
-              return (
-                <Grid item xs={12} sm={6} md={4} key={news?.news_id}>
-                  <NewsCard news={news} />
-                </Grid>
-              );
-            })}
-        </Grid>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          {loaderOnPage === "covid-news" && <CircularProgress />}
+        </Box>
+
+        {loaderOnPage !== "covid-news" && (
+          <Grid container rowSpacing={2} columnSpacing={2} sx={{ p: 2 }}>
+            {allCovidTableData?.length > 0 &&
+              allCovidTableData?.map((news) => {
+                return (
+                  <Grid item xs={12} sm={6} md={4} key={news?.news_id}>
+                    <NewsCard news={news} />
+                  </Grid>
+                );
+              })}
+          </Grid>
+        )}
+
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Pagination
+            count={8}
+            variant="outlined"
+            shape="rounded"
+            onChange={(event, value) => getAllCovidNewsData(value)}
+          />
+        </Box>
       </TabPanel>
 
       <TabPanel value={value} index={1}>
-        <Grid container rowSpacing={2} columnSpacing={2} sx={{ p: 2 }}>
-          {allHealthTableData.length > 0 &&
-            allHealthTableData.map((news) => {
-              return (
-                <Grid item xs={12} sm={6} md={4} key={news.news_id}>
-                  <NewsCard news={news} />
-                </Grid>
-              );
-            })}
-        </Grid>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          {loaderOnPage === "health-news" && <CircularProgress />}
+        </Box>
+
+        {loaderOnPage !== "health-news" && (
+          <Grid container rowSpacing={2} columnSpacing={2} sx={{ p: 2 }}>
+            {allHealthTableData.length > 0 &&
+              allHealthTableData.map((news) => {
+                return (
+                  <Grid item xs={12} sm={6} md={4} key={news.news_id}>
+                    <NewsCard news={news} />
+                  </Grid>
+                );
+              })}
+          </Grid>
+        )}
+
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Pagination
+            count={8}
+            variant="outlined"
+            shape="rounded"
+            onChange={(event, value) => getAllHealthNewsData(value)}
+          />
+        </Box>
       </TabPanel>
 
       <TabPanel value={value} index={2}>
-        <Grid container rowSpacing={2} columnSpacing={2} sx={{ p: 2 }}>
-          {allVaccineTableData.length > 0 &&
-            allVaccineTableData.map((news) => {
-              return (
-                <Grid item xs={12} sm={6} md={4} key={news.news_id}>
-                  <NewsCard news={news} />
-                </Grid>
-              );
-            })}
-        </Grid>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          {loaderOnPage === "vaccine-news" && <CircularProgress />}
+        </Box>
+
+        {loaderOnPage !== "vaccine-news" && (
+          <Grid container rowSpacing={2} columnSpacing={2} sx={{ p: 2 }}>
+            {allVaccineTableData.length > 0 &&
+              allVaccineTableData.map((news) => {
+                return (
+                  <Grid item xs={12} sm={6} md={4} key={news.news_id}>
+                    <NewsCard news={news} />
+                  </Grid>
+                );
+              })}
+          </Grid>
+        )}
+
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          {/* <Pagination
+            count={1}
+            variant="outlined"
+            shape="rounded"
+            onChange={(event, value) => getAllVaccineNewsData(value)}
+          /> */}
+        </Box>
       </TabPanel>
     </>
   );
